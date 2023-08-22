779b9093146c4d54ab90f5d0c6dea6c900d6bc71
Atul Kumar Giri <atulgiri21dec@gmail.com>
2023-08-22 16:04:57 +0530

    Linkes Added Header FIxed

--- src/components/frontend/layouts/Footer.js
+++ src/components/frontend/layouts/Footer.js
@@ -5,13 +5,16 @@ function Footer() {
     <div> 
         <footer className="footer bg-light ">
             <div className="up-foot d-md-flex justify-content-between align-items-center d-sm-block">
-                <div className="logo text-center">
+                <div className="logo text-center d-md-flex align-items-center d-sm-none">
                     <img src="./assets/images/website/logo.png" alt="" />
-                    <p>Atul Kumar Giri </p> 
-                    <span> Website Developer and Competitive Coder </span>
+                    <div className="title">
+                        <p>Atul Kumar Giri </p> 
+                        <span> Website Developer and Competitive Coder </span>
+                    </div>
                 </div>
+
                 <div className="copy text-center">
-                    <p>&copy; 2023 | <b> Atul Kumar Giri </b>| All Rights Reserved </p>
+                    <p>&copy; Copyright 2023 | Atul Kumar Giri </p>
                     <div className="design text-center">
                         <p><span> Designed & Developed by Atul Kumar Giri </span></p>
                     </div>
@@ -19,19 +22,17 @@ function Footer() {
                 <div className="social text-center">
                     <p className='mb-1'>Connect with me at</p>
                     <ul className='d-flex align-items-center p-0 m-0'>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-google"><i className="fa fa-google"></i></a></li>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-github"><i className="fa fa-github"></i></a></li>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-facebook"><i className="fa fa-facebook"></i></a></li>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-linkedin"><i className="fa fa-linkedin"></i></a></li>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-instagram"><i className="fa fa-instagram"></i></a></li>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-primary"><i className="fa fa-telegram"></i></a></li>
-                        <li className='mx-1'><a href="#" className="btn btn-social-icon  btn-success"><i className="fa fa-whatsapp"></i></a></li>
+                        <li className='mx-1'><a href="mailto:atulgiri21dec@gmail.com" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-google"><i className="fa fa-google"></i></a></li>
+                        <li className='mx-1'><a href="https://github.com/AtulKumarGiri" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-github"><i className="fa fa-github"></i></a></li>
+                        <li className='mx-1'><a href="https://www.facebook.com/profile.php?id=100057191202899" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-facebook"><i className="fa fa-facebook"></i></a></li>
+                        <li className='mx-1'><a href="https://www.linkedin.com/in/atul-kumar-giri-778665183/" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-linkedin"><i className="fa fa-linkedin"></i></a></li>
+                        <li className='mx-1'><a href="https://www.instagram.com/atul.kumar_giri/" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-instagram"><i className="fa fa-instagram"></i></a></li>
+                        <li className='mx-1'><a href="https://telegram.me/AtulKumarGiri" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-primary"><i className="fa fa-telegram"></i></a></li>
+                        <li className='mx-1'><a href="https://wa.me/+918481823182" target="_blank" rel="noreferrer" className="btn btn-social-icon  btn-success"><i className="fa fa-whatsapp"></i></a></li>
                     </ul>
                 </div>
             </div>
-            {/* <div className="down-foot text-center">
-                <p> Designed & Developed by Atul Kumar Giri</p>
-            </div> */}
+            
         </footer>
     </div>
   )
--- src/components/frontend/layouts/Header.js
+++ src/components/frontend/layouts/Header.js
@@ -12,39 +12,34 @@ import {
 
 
 function Header() {
-	const [isOpen, setIsOpen] = React.useState(false);
+
+    const[isOpen, setIsOpen] =React.useState(false);
 
 	return (
-		<div className="container" style={{
-			display: 'block'
-		}}>
-			{/* <h5>ReactJS Reactstrap Navbar Component</h5> */}
-			<Navbar light expand="md">
-				<NavbarBrand href="/"><img src="./assets/images/website/logo.png" alt="#logo" width={90} />  <span>Atul Kumar Giri</span></NavbarBrand>
+		<div className="container" style={{display: 'block' }}>
+			<Navbar light expand="md" >
+			    <NavbarBrand href="/"><img src="./assets/images/website/logo.png" alt="#logo" width={90} />  <span>Atul Kumar Giri</span></NavbarBrand>
 				<NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
-				<Collapse isOpen={isOpen} navbar className="justify-content-end">
-					<Nav className="mr-auto" navbar>
-						<NavItem>
-							<NavLink href="#">Home</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink href="#">About</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink href="#">Portfolio</NavLink>
-						</NavItem>
-                        <NavItem>
-							<NavLink href="#">Contact</NavLink>
-						</NavItem>
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="me-auto mb-2 mb-lg-0" navbar>
+                            <NavItem>
+                                <NavLink href="#">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">About</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Portfolio</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Contact</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Download Resume</NavLink>
+                            </NavItem>
 					</Nav>
+                    <Button className="btn btn-success">Hire Me</Button>
 				</Collapse >
-                <Collapse isOpen={isOpen} navbar className="justify-content-end">
-                    <Nav navbar>
-						<NavItem>
-                            <Button>Hire Me</Button>
-						</NavItem>
-                    </Nav>
-                </Collapse>
 			</Navbar>
 		</div >
 	);
