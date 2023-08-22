bcc279f805da407352984d00944e8f0e6734e6dd
Atul Kumar Giri <atulgiri21dec@gmail.com>
2023-08-22 17:59:29 +0530

    Home Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from './components/frontend/layouts/Footer';
 import Header from './components/frontend/layouts/Header';
+import Home from './components/frontend/Home';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/components/frontend/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Button} from 'reactstrap';
+
+function Home() {
+  return (
+    <div className='container-fluid'>
+        <div className="container home-container d-md-flex align-items-center justify-content-between d-sm-block ">
+            <div className="info">
+                <div className="content-heading">
+                    <h1>I'm Atul Kumar Giri</h1>
+                </div>
+                <div className="content">
+                    <p className='my-4 mb-5'>I have built many web applications and websites using Laravel for clients in different industries, and my portfolio demonstrates my expertise in Laravel development and delivering top-notch projects that fulfill my clients’ requirements.</p>
+
+                    <Button>Learn More</Button>
+                </div>
+            </div>
+            <div className="img text-right">
+                <img src="./assets/images/personal/2nd.png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
